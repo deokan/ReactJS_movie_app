936a15cc1ff309bcbaf4d44c70bc00f86e2dceb5
deokan <eundeok.yoon@gmail.com>
2019-02-07 21:00:01 +0900

    13. Smart vs Dumb Components

--- src/Movie.js
+++ src/Movie.js
@@ -1,34 +1,58 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-class Movie extends Component {
+// class Movie extends Component {
+//
+//   static propTypes = {
+//     title: PropTypes.string.isRequired,
+//     poster: PropTypes.string.isRequired
+//   }
+//
+//   render() {
+//     return (
+//       <div>
+//         <MoviePoster poster={this.props.poster}/>
+//         <h1>{this.props.title}</h1>
+//       </div>
+//     )
+//   }
+// }
 
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
-  }
-
-  render() {
-    return (
-      <div>
-        <MoviePoster poster={this.props.poster}/>
-        <h1>{this.props.title}</h1>
-      </div>
-    )
-  }
+function Movie({title, poster}){
+  return (
+    <div>
+      <MoviePoster poster={poster}/>
+      <h1>{title}</h1>
+    </div>
+  )
 }
 
-class MoviePoster extends Component {
-
-  static propTypes = {
-    poster: PropTypes.string.isRequired
-  }
-
-  render() {
-    return (
-      <img src={this.props.poster} width="300" height="430"/>
-    )
-  }
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired
 }
+
+// class MoviePoster extends Component {
+//   static propTypes = {
+//     poster: PropTypes.string.isRequired
+//   }
+//
+//   render() {
+//     return (
+//       <img src={this.props.poster} width="300" height="430"/>
+//     )
+//   }
+// }
+
+function MoviePoster({poster}) {
+  return (
+    <img src={poster} width="300" height="430"/>
+  )
+}
+
+MoviePoster.propTypes = {
+  poster: PropTypes.string.isRequired
+}
+
 export default Movie
