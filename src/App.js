084f42a6a613a558921b6d9341d3d62ddf1d0c9c
deokan <46529677+deokan@users.noreply.github.com>
2019-04-10 15:49:39 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,13 @@ class App extends Component {
   _renderMovies = () => {
     console.log('_renderMovies')
     const movies = this.state.movies.map(movie => {
-      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
+      return <Movie 
+        title={movie.title_english} 
+        poster={movie.medium_cover_image} 
+        genres={movie.genres}
+        synopsis={movie.synopsis}
+        key={movie.id} 
+      />
     })
     return movies
   }
